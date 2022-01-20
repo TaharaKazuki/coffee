240cd9543f79d4015df57c159987aaca2139bf60
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-01-20 11:43:07 +0900

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,6 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>Coffe Connosi</h1>
       </main>
-
       <footer className={styles.footer}></footer>
     </div>
   )
